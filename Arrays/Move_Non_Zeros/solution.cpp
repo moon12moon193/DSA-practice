ceb15f9350f28581c91625e539c01c9e7511ed19
mamoonaAbbas <mamoonaabbas1969@gmail.com>
2026-05-03 21:23:18 -0700

    Add moveZeroes function to rearrange array elements
    Implement moveZeroes function to shift non-zero elements to the front and fill the rest with zeros.

---
+++ Arrays/Move_Non_Zeros/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int j=0;
+
+        
+        for(int i=0;i<nums.size();i++){
+           if(nums[i]!=0 && j<nums.size()){
+            nums[j]=nums[i];
+            j++;
+
+           }
+        }
+        while(j<nums.size()){
+            nums[j]=0;
+            j++;
+        }
+        for(int val: nums){
+            cout<<val;
+        }
+        
+    }
+};
