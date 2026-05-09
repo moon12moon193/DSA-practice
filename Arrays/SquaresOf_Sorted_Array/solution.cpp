8d7647ac7676237a4e66b73af342c1bfae062c98
mamoonaAbbas <mamoonaabbas1969@gmail.com>
2026-05-09 10:28:28 -0700

    Refactor sortedSquares to avoid sorting(Optimized Approach)

--- Arrays/SquaresOf_Sorted_Array/solution.cpp
+++ Arrays/SquaresOf_Sorted_Array/solution.cpp
@@ -1,12 +1,45 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            nums[i]=nums[i]*nums[i];
+        int i=0;
+        while(i<nums.size() && nums[i]<0){
+            i++;
         }
-        sort(nums.begin(),nums.end());
-        return nums;
+        int j=i-1;
+        for(int k=0;k<nums.size();k++){
+            nums[k]=nums[k]*nums[k];
+        }
+        if(i==0){
+            return nums;
+        }else if(i==nums.size()){
+            reverse(nums.begin(),nums.end());
+            return nums;
+        }else{
+            vector<int>result(nums.size(),0);
+            int l=0;
+            while(i<nums.size() && j>=0){
+                if(nums[i]<nums[j]){
+                    result[l]=nums[i];
+                    i++;
+                    l++;
+                }else{
+                    result[l]=nums[j];
+                    j--;
+                    l++;
+                }
+            }
+            while(i<nums.size()){
+                result[l]=nums[i];
+                i++;
+                l++;
+            }
+            while(j>=0){
+                result[l]=nums[j];
+                j--;
+                l++;
+            }
+            return result;
+        }
         
     }
 };
